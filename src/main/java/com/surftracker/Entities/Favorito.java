fedc3ee8ac921d39e2e69259696b0d2176f1b4ff
leoben25 <147931123+leoben25@users.noreply.github.com>
2026-05-09 22:10:23 -0500

    Crea entidades favorito notificacion y feedback

---
+++ src/main/java/com/surftracker/Entities/Favorito.java
@@ -0,0 +1,31 @@
+package com.surftracker.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "favorito")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Favorito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_favorito")
+    private Integer idFavorito;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_localizacion", nullable = false)
+    private Localizacion localizacion;
+
+    @Column(name = "fecha_agregado")
+    private LocalDate fechaAgregado;
+}
