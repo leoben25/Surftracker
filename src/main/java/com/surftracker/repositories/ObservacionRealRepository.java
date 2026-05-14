140a9592237c113ec37d800db1030d2ec73eb8b8
RodrigoGit09 <u20211f052@upc.edu.pe>
2026-05-13 19:43:00 -0500

    entities y repository ConsultaPronostico

--- src/main/java/com/surftracker/repositories/ObservacionRealRepository.java
+++ src/main/java/com/surftracker/repositories/ObservacionRealRepository.java
@@ -6,8 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.sql.Date;
 import java.util.List;
 
-public interface ObservacionRealRepository
-        extends JpaRepository<ObservacionReal,Integer> {
+public interface ObservacionRealRepository extends JpaRepository<ObservacionReal,Integer> {
 
 
     List<ObservacionReal>
